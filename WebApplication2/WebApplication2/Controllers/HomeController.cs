117b121a9f1d23b42cf54facd399c750d33c382e
Matteo <matteolocher@gmail.com>
2018-01-17 13:04:30 +0100

    Initial Commit

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public IActionResult Index()
+        {
+            StatusMessage = $"Hello World {DateTime.UtcNow}";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
